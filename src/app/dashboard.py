175242bad059fdc246594b6ca73bd659a2f70bcb
Théo Px <magnum35puc@gmail.com>
2024-10-20 00:33:49 +0200

    packaging

--- src/app/dashboard.py
+++ src/app/dashboard.py
@@ -3,16 +3,11 @@ from .layout import layout  # Import the layout from layout.py
 from .callbacks import register_callbacks  # Import the callbacks from callbacks.py
 import dash_bootstrap_components as dbc
 
-# Initialize Dash app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-
-# Set the layout of the app
-app.layout = layout
-
-# Register the callbacks
-register_callbacks(app)  # Call the function to register all callbacks
-
-# Run the app
-if __name__ == '__main__':
+def main():
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+    app.layout = layout
+    register_callbacks(app)
     app.run_server(debug=True)
+
+if __name__ == "__main__":
+    main()
